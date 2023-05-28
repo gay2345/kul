be163d104bda4aef25815bf27e6499da7de4f1d4
gay2345 <samatovyrys09@gmail.com>
2023-05-28 21:21:55 +0600

    dz.3.m

---
+++ 1 hw/pon.js
@@ -0,0 +1,48 @@
+//HW-1
+//old
+// function printArrayValues(arr) {
+//  for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i], 'array');
+//    }
+//   }
+
+//new
+// let printArrayValues = 0
+// function recursion(){
+//     printArrayValues++;
+//     console.log(printArrayValues, 'array');
+//     if(printArrayValues === 3){
+//         return;
+//     }else{
+//         return recursion();
+//     }
+// }
+// recursion()
+
+
+
+//HW-2
+// const symbols = 'My name 457 is Alex'
+// const regex = /\w/g;
+// const result = symbols.match(regex);
+// console.log(result);
+
+
+//HW-3
+// const symbols = 'Hello\tWorld\nTest'
+// const regex = /\s/g;
+// const result = symbols.match(regex);
+// console.log(result);
+
+
+//HW-4 
+// function countChar(str, char) {
+//     let count = 0;
+//     for (let i = 0; i < str.length; i++) {
+//       if (str[i] === char) {
+//         count++;
+//       }
+//     }
+//     return count;
+//   }
+//   console.log(countChar('loremipsumdolor', 'o'));
